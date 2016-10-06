a87f0503612f445132566b3059b4dfbb0c695a20
Abdelhakim Akodadi <akodadi.abdelhakim@gmal.com>
2016-10-06 02:10:47 +0100

    Add input neural trained for two test

--- test/neural_test.js
+++ test/neural_test.js
@@ -32,4 +32,19 @@ QUnit.module("Neural", function () {
     neural.input({mother: true, father:true});
     assert.equal(neural.output(), false, "Trained neural for one value must output the same value regardless of the input");
   });
+  QUnit.test("Trained input neural test for two values", function(assert) {
+    var neural = new Neural({father: null, mother: null});
+    neural.input({mother: false, father:false});
+    neural.train(false);
+    neural.input({mother: true, father:true});
+    neural.train(true);
+    neural.input({mother: false, father:false});
+    assert.equal(neural.output(), false, "Trained neural for two values must output the value of the closest input trained for");
+    neural.input({mother: false, father:true});
+    assert.equal(neural.output(), false, "Trained neural for two values must output the value of the closest input trained for");
+    neural.input({mother: true, father:false});
+    assert.equal(neural.output(), true, "Trained neural for two values must output the value of the closest input trained for");
+    neural.input({mother: true, father:true});
+    assert.equal(neural.output(), true, "Trained neural for two values must output the value of the closest input trained for");
+  });
 });
